6a0d5768d95aec93d17703f6da0ca67083bc7ed2
gabbyblake <gaby.blake2@gmail.com>
2021-11-11 12:45:22 -0500

    updated pieces and simple test stepper code

--- PIE Final Test Files/sprint1-test_stepper/Sprint1-control_stepper.py
+++ PIE Final Test Files/sprint1-test_stepper/Sprint1-control_stepper.py
@@ -1,18 +1,16 @@
 from serial import Serial, SerialException
 
-cxn = Serial('/dev/tty.usbmodem1411', baudrate=9600)
+cxn = Serial('COM11', baudrate=9600)
 
-while(True):    
-    try:        
-        cmd_id = int(input("Please enter the number of steps to move the motor (1-200 inclusive "))        
-        if int(cmd_id) > 200 or int(cmd_id) < 1:            
+while(True):            
+        cmd_id = int(input("Please enter the number of steps to move the motor (1-200 inclusive): "))        
+        print(cmd_id)
+        if int(cmd_id) > 2000 or int(cmd_id) < 1:            
             print("Please insert a valid integer.")       
         else:            
-            cxn.write([int(cmd_id)])            
-
+            cxn.write([int(cmd_id)])
             while cxn.inWaiting() < 1:                
-                pass            
+                pass                       
             result = cxn.readline();            
             print(result)    
-    except ValueError:        
-        print("You must enter an integer value between 1 and 2.")
+    
