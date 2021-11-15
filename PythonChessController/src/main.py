94987e389cc5160113dad67388efd55ce745bcd0
benmor20 <benmor2020@gmail.com>
2021-11-15 12:21:04 -0500

    Stepper motor integration

--- PythonChessController/src/main.py
+++ PythonChessController/src/main.py
@@ -1,7 +1,18 @@
 """
 Main script to control the chess board
 """
+from src.hardware.drive_system import Stepper
+from src.hardware.serial_protocol import Serial
 
 
 if __name__ == '__main__':
-    print('Main')
+    serial = Serial()
+    serial.wait_for_setup()
+    print('Setup done')
+    stepper = Stepper(serial, 0)
+    stepper.set_accel_limit(100)
+    stepper.set_power(1)
+    stepper.increment_pos(10000)
+    stepper.update()
+    while True:
+        print(f'From serial: {serial.bridge.readline().decode()}')
