fd5b7eb6df11d2f7a7613a0e6b6e1583661aa248
gabbyblake <gaby.blake2@gmail.com>
2021-12-09 10:21:06 -0500

    tested stuff

--- PythonChessController/src/testing.py
+++ PythonChessController/src/testing.py
@@ -4,19 +4,20 @@ from time import sleep
 
 
 if __name__ == '__main__':
-    s1 = Serial('COM13')
+    s1 = Serial('COM6')
     s1.wait_for_setup()
-    s2 = Serial('COM14')
+    s2 = Serial('COM11')
     s2.wait_for_setup()
-    step = Stepper(s2, 0)
+    step1 = Stepper(s1, 0)
+    step2 = Stepper(s2, 0)
 
-    s1.set_value(13, True)
-    step.move_to_pos(1000)
-    step.update()
+    s2.set_value(13, True)
+    step2.move_to_pos(1000)
+    step2.update()
     sleep(2)
 
     while True:
-        s1.set_value(13, False)
+        s2.set_value(13, False)
         sleep(0.5)
-        s1.set_value(13, True)
+        s2.set_value(13, True)
         sleep(0.5)
