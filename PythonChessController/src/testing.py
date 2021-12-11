76cdbdc025434af11145094441431323f1d4e853
Carlokko <Carlokko16@gmail.com>
2021-12-11 12:02:03 -0500

    controller fixes

--- PythonChessController/src/testing.py
+++ PythonChessController/src/testing.py
@@ -6,5 +6,7 @@ from time import sleep
 if __name__ == '__main__':
     serial = Serial()
     stepper = Stepper(serial, 1)
+    stepper = Stepper(serial, 1)
+    stepper.set_power(.25)
     stepper.move_to_pos(50)
     stepper.update()
